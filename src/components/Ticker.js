fe8f93c8a4c6f0970746806cc8270eb4a445b7c0
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-09-04 14:56:48 +0300

    <The useRef Hook>

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -6,18 +6,30 @@ function Ticker() {
   const [color, setColor] = useState("black");
 
   useEffect(() => {
-    const id = setInterval(() => setPrice(makeRandomNumber), 1000);
-    return function () {
+    const id = setInterval(() => {
+      const newPrice = makeRandomNumber();
+      if (newPrice > price) {
+        setColor("green");
+      } else if (newPrice < price) {
+        setColor("red");
+      } else {
+        setColor("black");
+      }
+      setPrice(newPrice);
+    }, 1000);
+
+    return function cleanup() {
       clearInterval(id);
     };
-  }, []);
+  }, [price]);
 
   return (
     <div>
       <h1>TickerMaster</h1>
-      <h2 style={{ color: color }}>Price: ${price}</h2>
+      <h2 style={{ color: color }}>Price: ${price.toFixed(2)}</h2>
     </div>
   );
 }
 
 export default Ticker;
+
